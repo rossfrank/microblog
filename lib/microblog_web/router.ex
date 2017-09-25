4f90481e9a9a34317ba4ef739621b42bb1f197c8
rossfrank <rossfrank27@gmail.com>
2017-09-25 17:22:04 -0400

    first commit

---
+++ lib/microblog_web/router.ex
@@ -0,0 +1,26 @@
+defmodule MicroblogWeb.Router do
+  use MicroblogWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", MicroblogWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", MicroblogWeb do
+  #   pipe_through :api
+  # end
+end

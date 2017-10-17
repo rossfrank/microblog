0164d86d3422faa586a4a0f9031580fd0325ca0f
rossfrank <rossfrank27@gmail.com>
2017-10-17 19:39:49 -0400

    live updates

--- lib/microblog_web/channels/user_socket.ex
+++ lib/microblog_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule MicroblogWeb.UserSocket do
 
   ## Channels
   # channel "room:*", MicroblogWeb.RoomChannel
+  channel "updates:lobby", MicroblogWeb.UpdatesChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
